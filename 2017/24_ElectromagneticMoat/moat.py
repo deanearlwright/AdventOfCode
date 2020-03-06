9b95942ef506a7b47403677af8cfe7de2855745d
deanearlwright@github.com <github@deanearlwright.gmail>
2020-03-06 17:42:44 -0500

    Completed 2017 Day 24 Parts 1 and 2

--- 2017/24_ElectromagneticMoat/moat.py
+++ 2017/24_ElectromagneticMoat/moat.py
@@ -13,6 +13,8 @@
 # ----------------------------------------------------------------------
 #                                                                 import
 # ----------------------------------------------------------------------
+from collections import defaultdict
+from random import choice
 
 # ----------------------------------------------------------------------
 #                                                              constants
@@ -31,23 +33,233 @@ class Moat(object):   # pylint: disable=R0902, R0205
         # 1. Set the initial values
         self.part2 = part2
         self.text = text
+        self.components = []
+        self.ports = defaultdict(list)
 
         # 2. Process text (if any)
         if text is not None:
-            pass
+            self.process_text(text)
+
+    def process_text(self, text):
+        "Populate components and ports from text"
+
+        # 1. Loop for all of the components in the text
+        for cnum, component in enumerate(text):
+
+            # 2. Get the port sizes of the component
+            ports = [int(x) for x in component.split('/')]
+
+            # 3. Add the component to the list
+            self.components.append((ports[0], ports[1]))
+
+            # 4. Add the port numbers to the ports index
+            self.ports[ports[0]].append(cnum)
+            if ports[1] != ports[0]:
+                self.ports[ports[1]].append(cnum)
+
+    @staticmethod
+    def other_port(component, port):
+        "Return the port at the other end of the component"
+
+        if port == component[0]:
+            return component[1]
+        return component[0]
+
+    def last_port(self, bridge):
+        "Return the bridges' empty port"
+
+        # 1. Assume we are just starting
+        result = 0
+
+        # 1. If no bridge yet, need magnetic end
+        if not bridge:
+            return result
+
+        # 2. Loop for all of the component of the bridge
+        for component in bridge:
+
+            # 3. Get the port number of the far side of this component
+            result = Moat.other_port(self.components[component], result)
+
+        # 4. Return the final port
+        return result
+
+    def build_a_bridge(self, first=False):
+        "Create a bridge out of components"
+
+        # 1. Start with nothing
+        bridge = []
+        need = 0
+
+        # 2. Add components until we can add no other
+        while True:
+
+            # 3. Try to add another component
+            component = self.pick_a_component(bridge=bridge, need=need, first=first)
+
+            # 4. If no component to add, return the bridge that we have
+            if component is None:
+                return bridge
+
+            # 5. What port will we need next
+            need = Moat.other_port(self.components[component], need)
+
+            # 6. Add component to bridge and try again
+            bridge.append(component)
+
+    def pick_a_component(self, bridge=None, need=None, first=False):
+        "Add another component to the bridge"
+
+        # 1. If there is no bridge, start one
+        if bridge is None:
+            bridge = []
+            need = 0
+
+        # 2. If need is unkown, determine it
+        if need is None:
+            need = self.last_port(bridge)
+
+        # 3. Get the list of components that match the port that we need
+        components = self.ports[need]
+
+        # 4. Can only pick from the ones not already used
+        pickable = [x for x in components if x not in bridge]
+
+        # 5. If nothing to choose from, return failure
+        if not pickable:
+            return None
+
+        # 6. Else select a the first (or random) component that fits
+        if first:
+            return pickable[0]
+        return choice(pickable)
+
+    def all_bridges(self, bridge=None):
+        "Return all possible bridges, one by one"
+
+        # 1. Start with an empty bridge if none is given
+        if bridge is None:
+            bridge = []
+
+        # 2. Loop for all components that can continue the bridge
+        for component in self.all_components(bridge):
+
+            # 2. Add component to the bridge
+            bridge.append(component)
+
+            # 3. Return this bridge
+            yield bridge
+
+            # 4. Make more bridges start with the this one
+            yield from self.all_bridges(bridge=bridge)
+
+            # 5. Remove the added component
+            bridge.pop(-1)
+
+    def all_components(self, bridge=None, need=None):
+        "Iterate over matching components"
+
+        # 1. If there is no bridge, make one
+        if bridge is None:
+            bridge = []
+            need = 0
+
+        # 2. If need is unkown, determine it
+        if need is None:
+            need = self.last_port(bridge)
+
+        # 3. Get the list of components that match the port that we need
+        components = self.ports[need]
+
+        # 4. Can only pick from the ones not already used
+        pickable = [x for x in components if x not in bridge]
+
+        # 5. Return the components one at a time
+        for component in pickable:
+            yield component
+
+    def check_bridge(self, bridge):
+        "Check the structural integrity of the bridge"
+
+        # 1. Bridges must have at least one component
+        if not bridge:
+            return False
+
+        # 2. Keep track of the components used and the ports needed
+        used = set()
+        need = 0
+
+        # 3. Loop for all of the components in the bridge
+        for component in bridge:
+
+            # 4. It must not have been used before
+            if component in used:
+                return False
+            used.add(component)
+
+            # 5. The component must satisfy the need
+            if need not in self.components[component]:
+                return False
+
+            # 6. Determine the next need
+            need = Moat.other_port(self.components[component], need)
+
+        # 7. Look good to me
+        return True
+
+    def strength(self, bridge):
+        "Determine the strength of the bridge"
+
+        return sum([sum(self.components[c]) for c in bridge])
 
     def part_one(self, verbose=False, limit=0):
         "Returns the solution for part one"
 
-        # 1. Return the solution for part one
-        return None
+        # 1. Remember the strongest
+        result = -1
 
+        # 2. Loop for all the bridges
+        for bridge in self.all_bridges():
+
+            # 3. Determine the strength of the bridge
+            strength = self.strength(bridge)
+
+            # 4. Remember the highest stength
+            if strength > result:
+                result = strength
+
+        # 5. Return the highest strength found
+        if result == -1:
+            return None
+        return result
 
     def part_two(self, verbose=False, limit=0):
         "Returns the solution for part two"
 
-        # 1. Return the solution for part two
-        return None
+        # 1. Remember the strongest and the longest
+        result = -1
+        longest = -1
+
+        # 2. Loop for all the bridges
+        for bridge in self.all_bridges():
+
+            # 3. Determine the strength of the bridge
+            strength = self.strength(bridge)
+
+            # 4. If this isn't the longest, ignore it
+            if len(bridge) < longest:
+                continue
+            longest = len(bridge)
+
+            # 5. Remember the highest stength
+            if strength > result:
+                result = strength
+
+        # 6. Return the highest strength of the longest bridge found
+        if result == -1:
+            return None
+        return result
+
 
 # ----------------------------------------------------------------------
 #                                                  module initialization
--- 2017/24_ElectromagneticMoat/test_moat.py
+++ 2017/24_ElectromagneticMoat/test_moat.py
@@ -21,12 +21,46 @@ import moat
 # ----------------------------------------------------------------------
 #                                                              constants
 # ----------------------------------------------------------------------
-EXAMPLE_TEXT = ""
-PART_ONE_TEXT = ""
-PART_TWO_TEXT = ""
+EXAMPLE_TEXT = """
+0/2
+2/2
+2/3
+3/4
+3/5
+0/1
+10/1
+9/10
+"""
 
-PART_ONE_RESULT = None
-PART_TWO_RESULT = None
+
+BRIDGES = [
+    [5],  # - 0/1
+    [5, 6],  # - 0/1--10/1
+    [5, 6, 7],  # - 0/1--10/1--9/10
+    [0],  # - 0/2
+    [0, 2],  # - 0/2--2/3
+    [0, 2, 3],  # - 0/2--2/3--3/4
+    [0, 2, 4],  # - 0/2--2/3--3/5
+    [0, 1],  # - 0/2--2/2
+    [0, 1, 2],  # - 0/2--2/2--2/3
+    [0, 1, 2, 3],  # - 0/2--2/2--2/3--3/4
+    [0, 1, 2, 4],  # - 0/2--2/2--2/3--3/5
+]
+STRENGTHS = [1, 12, 31, 2, 7, 14, 15, 6, 11, 18, 19]
+LAST = [1, 10, 9, 2, 3, 4, 5, 2, 3, 4, 5]
+
+
+BAD_BRIDGES = [
+    [],  # No components
+    [0, 1, 0],  # Reuses a component
+    [0, 1, 6],  # Non matching ports
+]
+
+PART_ONE_TEXT = EXAMPLE_TEXT
+PART_TWO_TEXT = EXAMPLE_TEXT
+
+PART_ONE_RESULT = 31
+PART_TWO_RESULT = 19
 
 # ======================================================================
 #                                                               TestMoat
@@ -45,6 +79,8 @@ class TestMoat(unittest.TestCase):  # pylint: disable=R0904
         # 2. Make sure it has the default values
         self.assertEqual(myobj.part2, False)
         self.assertEqual(myobj.text, None)
+        self.assertEqual(myobj.components, [])
+        self.assertEqual(myobj.ports, {})
 
     def test_text_init(self):
         "Test the Moat object creation from text"
@@ -54,22 +90,128 @@ class TestMoat(unittest.TestCase):  # pylint: disable=R0904
 
         # 2. Make sure it has the expected values
         self.assertEqual(myobj.part2, False)
-        self.assertEqual(myobj.text, EXAMPLE_TEXT)
+        self.assertEqual(len(myobj.text), 8)
+        self.assertEqual(len(myobj.components), 8)
+        self.assertEqual(myobj.components[0], (0, 2))
+        self.assertEqual(len(myobj.ports), 8)
+        self.assertEqual(myobj.ports[0], [0, 5])
+
+    def test_strength(self):
+        "Test the Moat.strength function"
+
+        # 1. Create Moat object from text
+        myobj = moat.Moat(text=aoc_24.from_text(EXAMPLE_TEXT))
+
+        # 2. Loop for all of the example bridges
+        for bnum, bridge in enumerate(BRIDGES):
+
+            # 3. Compute and compare strengths
+            self.assertEqual(myobj.strength(bridge), STRENGTHS[bnum])
+
+    def test_check_bridge(self):
+        "Test the Moat.strength function"
+
+        # 1. Create Moat object from text
+        myobj = moat.Moat(text=aoc_24.from_text(EXAMPLE_TEXT))
+
+        # 2. Loop for all of the example bridges
+        for bridge in BRIDGES:
+
+            # 3. They should all pass the test
+            self.assertEqual(myobj.check_bridge(bridge), True)
+
+        # 4. Loop for all of the incorrectly assembled bridges
+        for bridge in BAD_BRIDGES:
+
+            # 5. They should all fail the test
+            self.assertEqual(myobj.check_bridge(bridge), False)
+
+    def test_simple_bridges(self):
+        "Test build a couple of bridges"
+
+        # 1. Create Moat object from text
+        myobj = moat.Moat(text=aoc_24.from_text(EXAMPLE_TEXT))
+
+        # 2. Build one bridge
+        bridge = myobj.build_a_bridge(first=True)
+
+        # 3. Validate it
+        self.assertEqual(myobj.check_bridge(bridge), True)
+        self.assertEqual(bridge, [0, 1, 2, 3])
+
+        # 4. Build a couple of random bridges
+        for _ in range(50):
+
+            # 5. Build a bridge
+            bridge = myobj.build_a_bridge()
+
+            # 6. Validate it
+            self.assertEqual(myobj.check_bridge(bridge), True)
+            self.assertTrue(bridge in BRIDGES)
+
+    def test_last_port(self):
+        "Test the Moat last_port function"
+
+        # 1. Create Moat object from text
+        myobj = moat.Moat(text=aoc_24.from_text(EXAMPLE_TEXT))
+
+        # 2. Loop for all of the example bridges
+        for bnum, bridge in enumerate(BRIDGES):
+
+            # 3. Verify that hey have the expected last port
+            self.assertEqual(myobj.last_port(bridge), LAST[bnum])
+
+    def test_all_components(self):
+        "Test the Moat all_components function"
+
+        # 1. Create Moat object from text
+        myobj = moat.Moat(text=aoc_24.from_text(EXAMPLE_TEXT))
+
+        # 2. Components that can start a bridge
+        self.assertEqual(list(myobj.all_components()), [0, 5])
+
+        # 3. Components that can be the used after 0/1 starts a bridge
+        self.assertEqual(list(myobj.all_components(bridge=[5])), [6])
+
+        # 4. Components that can be the used after 0/1--10/1 starts a bridge
+        self.assertEqual(list(myobj.all_components(bridge=[5, 6])), [7])
+
+        # 5. Components that can be the used after 0/1--10/1--9/10 starts a bridge
+        self.assertEqual(list(myobj.all_components(bridge=[5, 6, 7])), [])
+
+    def test_all_bridges(self):
+        "Test the Moat all_components function"
+
+        # 1. Create Moat object from text
+        myobj = moat.Moat(text=aoc_24.from_text(EXAMPLE_TEXT))
+
+        # 2. Loop for all the bridges
+        knt_bridges = 0
+        for bridge in myobj.all_bridges():
+
+            # 3. Validate it
+            self.assertEqual(myobj.check_bridge(bridge), True)
+            self.assertTrue(bridge in BRIDGES)
+
+            # 4. Keep track of the number of bridges generated
+            knt_bridges += 1
+
+        # 5. Verify that we generated them all
+        self.assertEqual(knt_bridges, len(BRIDGES))
 
     def test_part_one(self):
         "Test part one example of Moat object"
 
-        # 1. Create Spinlock object from text
+        # 1. Create Moat object from text
         myobj = moat.Moat(text=aoc_24.from_text(PART_ONE_TEXT))
 
         # 2. Check the part one result
         self.assertEqual(myobj.part_one(verbose=False), PART_ONE_RESULT)
 
-
     def test_part_two(self):
         "Test part two example of Moat object"
 
-        # 1. Create Spinlock object from text
+        # 1. Create Moat object from text
         myobj = moat.Moat(part2=True, text=aoc_24.from_text(PART_TWO_TEXT))
 
         # 2. Check the part two
