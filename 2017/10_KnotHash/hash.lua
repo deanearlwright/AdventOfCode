e9ca790b63f60211fd47c400a5deb75ab6d36ce3
deanearlwright <github@deanearlwright.gmail>
2021-11-30 20:20:07 -0500

    2017 day 10 lua initial

---
+++ 2017/10_KnotHash/hash.lua
@@ -0,0 +1,109 @@
+-- ======================================================================
+-- Knot Hash
+--   Advent of Code 2017 Day 10 -- Eric Wastl -- https://adventofcode.com
+--
+-- lua implementation by Dr. Dean Earl Wright III
+-- ======================================================================
+
+-- ======================================================================
+--                         h a s h . l u a
+-- ======================================================================
+-- A solver for the Advent of Code 2017 Day 10 puzzle
+
+-- ----------------------------------------------------------------------
+--                                                                  local
+-- ----------------------------------------------------------------------
+local Hash = { part2 = false, text = {}, length = 0, numbers = {}, lengths = {} }
+
+-- ----------------------------------------------------------------------
+--                                                              constants
+-- ----------------------------------------------------------------------
+ADDITIONAL_LENGTHS = "17, 31, 73, 47, 23"
+DEFAULT_LENGTH = 256
+
+-- ======================================================================
+--                                                                   Hash
+-- ======================================================================
+
+-- Object for KnotHash
+
+function Hash:Hash(o)
+
+  -- 1. Set the initial values
+  o = o or {}
+  o.part2 = o.part2 or false
+  o.text = o.text or {}
+  o.length = o.length or DEFAULT_LENGTH
+  o.numbers = {}
+  o.lengths = {}
+
+  -- 2. Create the object metatable
+  self.__index = self
+  setmetatable(o, self)
+
+  -- 3. Process text (if any)
+  if o.text ~= nil and #o.text > 0 then
+     o:_process_text(o.text[1])
+  end
+
+  -- 4. Fill in the numbers
+  if o.length > 0 then
+    for number = 0, o.length - 1 do
+      table.insert(o.numbers, number)
+    end
+  end
+  
+  -- 5. Return the Hash object
+  return o
+end
+
+function Hash:_process_text(line)
+  -- Assign values from text
+
+  -- 0. Precondition axioms
+  assert(line ~= nil and #line > 0)
+
+  -- 1. Loop for each number in the line
+  for number in string.gmatch(line, "%d+") do
+
+    -- 2. Add the number to the lengths
+    table.insert(self.lengths, tonumber(number))
+    end
+end
+
+function Hash:part_one(args)
+  -- Returns the solution for part one
+
+  -- 0. Precondition axioms
+  local verbose = args.verbose or false
+  local limit = args.limit or 0
+  assert(verbose == true or verbose == false)
+  assert(limit >= 0)
+
+  -- 1. Return the solution for part one
+  return nil
+end
+
+
+function Hash:part_two(args)
+  -- Returns the solution for part two
+
+  -- 0. Precondition axioms
+  local verbose = args.verbose or false
+  local limit = args.limit or 0
+  assert(verbose == true or verbose == false)
+  assert(limit >= 0)
+
+  -- 1. Return the solution for part two
+  return nil
+end
+
+-- ----------------------------------------------------------------------
+--                                                  module initialization
+-- ----------------------------------------------------------------------
+
+return Hash
+
+-- ======================================================================
+-- end                         h a s h . l u a                        end
+-- ======================================================================
---
+++ 2017/10_KnotHash/test_hash.lua
@@ -0,0 +1,132 @@
+-- ======================================================================
+-- Knot Hash
+--   Advent of Code 2017 Day 10 -- Eric Wastl -- https://adventofcode.com
+--
+-- lua implementation by Dr. Dean Earl Wright III
+-- ======================================================================
+
+-- ======================================================================
+--                    t e s t _ h a s h . l u a
+-- ======================================================================
+-- Test solver for Advent of Code 2017 day 10, Knot Hash
+
+-- ----------------------------------------------------------------------
+--                                                                require
+-- ----------------------------------------------------------------------
+local luaunit = require('luaunit')
+
+local hash = require('hash')
+
+-- ----------------------------------------------------------------------
+--                                                              from_text
+-- ----------------------------------------------------------------------
+
+function from_text(text)
+  -- Break the text into trimed, non-comment lines
+
+  -- 1. We start with no lines
+  local result = {}
+
+  -- 2. Loop for lines in the text
+  for line in text:gmatch('[^\r\n]+') do
+
+    -- 3. But ignore blank and comment lines
+    line = line:gsub("%s*$", "")
+    if #line > 0 and "!" ~= line:sub(1, 1) then
+
+      -- 4. Add the line
+      table.insert(result, line)
+    end
+  end
+
+  -- 5. Return a table of cleaned lines
+  return result
+end
+
+-- ----------------------------------------------------------------------
+--                                                               dict_len
+-- ----------------------------------------------------------------------
+function dict_len(d)
+    local result = 0
+    for _ in pairs(d) do 
+      result = result + 1
+    end
+    return result
+end
+
+-- ----------------------------------------------------------------------
+--                                                              constants
+-- ----------------------------------------------------------------------
+local EXAMPLE_TEXT = [[
+3, 4, 1, 5
+]]
+local PART_ONE_TEXT = EXAMPLE_TEXT
+local PART_TWO_TEXT = ""
+
+local PART_ONE_RESULT = nil
+local PART_TWO_RESULT = nil
+
+-- ======================================================================
+--                                                               TestHash
+-- ======================================================================
+
+function test_empty_init()
+  -- Test the default Hash creation
+
+  -- 1. Create default Hash object
+  local myobj = hash:Hash()
+
+  -- 2. Make sure it has the default values
+  luaunit.assertEquals(myobj.part2, false)
+  luaunit.assertEquals(#myobj.text, 0)
+  luaunit.assertEquals(myobj.length, 256)
+  luaunit.assertEquals(#myobj.numbers, 256)
+  luaunit.assertEquals(#myobj.lengths, 0)
+
+end
+
+function test_text_init()
+  -- Test the Hash object creation from text
+
+  -- 1. Create Hash object from text
+  local myobj = hash:Hash({text=from_text(EXAMPLE_TEXT), length=5})
+
+  -- 2. Make sure it has the expected values
+  luaunit.assertEquals(myobj.part2, false)
+  luaunit.assertEquals(#myobj.text, 1)
+  luaunit.assertEquals(myobj.length, 5)
+  luaunit.assertEquals(#myobj.numbers, 5)
+  luaunit.assertEquals(#myobj.lengths, 4)
+
+end
+
+function test_part_one()
+  -- Test part one example of Hash object
+
+  -- 1. Create Hash object from text
+  local myobj = hash:Hash({text=from_text(PART_ONE_TEXT)})
+
+  -- 2. Check the part one result
+  luaunit.assertEquals(myobj:part_one({verbose=false, limit=0}), PART_ONE_RESULT)
+
+end
+
+function test_part_two()
+  -- Test part two example of Hash object
+
+  -- 1. Create Hash object from text
+  local myobj = hash:Hash({part2=true, text=from_text(PART_TWO_TEXT)})
+
+  -- 2. Check the part two result
+  luaunit.assertEquals(myobj:part_two({verbose=false, limit=0}), PART_TWO_RESULT)
+
+end
+
+-- ----------------------------------------------------------------------
+--                                                  module initialization
+-- ----------------------------------------------------------------------
+os.exit( luaunit.LuaUnit.run() )
+
+-- ======================================================================
+-- end                    t e s t _ h a s h . l u a                   end
+-- ======================================================================
